691a8301bbe0bf6aac80fad34ac63cd41a5279be
Isanka Suresh <isankasuresh001@gmail.com>
2025-01-25 12:07:58 +0530

    implement customer create

--- _init_.py
+++ _init_.py
@@ -1,4 +1,5 @@
 from product import Product
+from customer import Customer
 
 # product = Product("laptop",36000,5)
 # print(product.get_details())
@@ -6,10 +7,18 @@ from product import Product
 # product.update_price(450000)
 # print(product.get_details())
 
-name = input("Enter product name: ")
-price = float(input("Enter product price: "))
-quantity = int(input("Enter product quantity: "))
+# name = input("Enter product name: ")
+# price = float(input("Enter product price: "))
+# quantity = int(input("Enter product quantity: "))
 
-product1 = Product(name, price, quantity)
+# product1 = Product(name, price, quantity)
+# print()
+# print(product1.get_details())
+
+name = input("Enter customer name: ")
+address = input("Enter customer address: ")
+contact = input("Enter customer contact: ")
+
+customer = Customer(name, address, contact)
 print()
-print(product1.get_details())
+print(customer.get_details())

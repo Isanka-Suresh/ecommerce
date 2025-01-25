b1c39a1f30a7b4f04add6396dd820a6b13927339
Isanka Suresh <isankasuresh001@gmail.com>
2025-01-25 12:05:13 +0530

    create product using inputs

--- _init_.py
+++ _init_.py
@@ -1,7 +1,15 @@
 from product import Product
 
-product = Product("laptop",36000,5)
-print(product.get_details())
+# product = Product("laptop",36000,5)
+# print(product.get_details())
 
-product.update_price(450000)
-print(product.get_details())
+# product.update_price(450000)
+# print(product.get_details())
+
+name = input("Enter product name: ")
+price = float(input("Enter product price: "))
+quantity = int(input("Enter product quantity: "))
+
+product1 = Product(name, price, quantity)
+print()
+print(product1.get_details())
